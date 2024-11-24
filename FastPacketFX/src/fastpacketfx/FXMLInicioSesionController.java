044d9df7b7e3eddb529efe7130ccb8f356182a9b
Rafael Vázquez Pérez <j.oskar.v282143@gmail.com>
2024-11-23 20:21:54 -0600

    Interfaz

--- FastPacketFX/src/fastpacketfx/FXMLInicioSesionController.java
+++ FastPacketFX/src/fastpacketfx/FXMLInicioSesionController.java
@@ -5,22 +5,31 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class FXMLInicioSesionController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private TextField tf_numeroPersonal;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private PasswordField pf_password;
+    @FXML
+    private Label lb_numeroFaltante;
+    @FXML
+    private Label lb_passwordFaltante;
+    @FXML
+    private Button btn_Ingresar;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onClickIngresar(ActionEvent event) {
+    }
     
 }
