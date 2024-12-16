31f8ec01ce77784a5c6fb575ce6f12be1dd2ed6a
Rafael Vázquez Pérez <j.oskar.v282143@gmail.com>
2024-12-15 21:19:42 -0600

    Unidad

--- FastPacketFX/src/fastpacketfx/FXMLEscenarioUnidadesController.java
+++ FastPacketFX/src/fastpacketfx/FXMLEscenarioUnidadesController.java
@@ -1,21 +1,73 @@
 package fastpacketfx;
 
+import fastpacketfx.modelo.dao.ColaboradorDAO;
+import fastpacketfx.modelo.dao.UnidadDAO;
+import fastpacketfx.pojo.Colaborador;
+import fastpacketfx.pojo.Unidad;
+import fastpacketfx.utilidades.Utilidades;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLEscenarioUnidadesController implements Initializable {
 
+    private ObservableList<Unidad> unidades;
     @FXML
     private TextField tf_buscar;
+    @FXML
+    private TableView<Unidad> tvUnidad;
+    @FXML
+    private TableColumn tcVin;
+    @FXML
+    private TableColumn tcNii;
+    @FXML
+    private TableColumn tcModelo;
+    @FXML
+    private TableColumn tcAnio;
+    @FXML
+    private TableColumn tcMarca;
+    @FXML
+    private TableColumn tcTipo;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       configurarTabla();
+       cargarInformacionTabla();
+    }
+
+    private void configurarTabla(){
+        tcVin.setCellValueFactory(new PropertyValueFactory("vin"));
+        tcNii.setCellValueFactory(new PropertyValueFactory("nii"));
+        tcModelo.setCellValueFactory(new PropertyValueFactory("modelo"));
+        tcAnio.setCellValueFactory(new PropertyValueFactory("anio"));
+        tcMarca.setCellValueFactory(new PropertyValueFactory("marca"));
+        tcTipo.setCellValueFactory(new PropertyValueFactory("tipo"));
+    }
+    
+    private void cargarInformacionTabla(){
+        //no se inicializa con new sino con: FXCollections
+        unidades = FXCollections.observableArrayList();
+        //obtener lo que trae el dao
+        System.out.print("Entrando a la lista");
+        List<Unidad> listaWS = UnidadDAO.obtenerColaboradores();
+        if(listaWS != null){
+            System.out.print("Entando a la condicion de busqueda");
+            unidades.addAll(listaWS);
+            tvUnidad.setItems(unidades);
+        }else{
+            Utilidades.mostrarAlertaSimple("Datos no disponible", "Lo sentimos, por el momento no se puede cargar la info de colaboradores", Alert.AlertType.ERROR);
+        }
+    }
 
     @FXML
     private void onClickBuscar(ActionEvent event) {
---
+++ FastPacketFX/src/fastpacketfx/pojo/Unidad.java
@@ -0,0 +1,100 @@
+package fastpacketfx.pojo;
+
+public class Unidad {
+    private Integer idUnidad;
+    private String vin;
+    private String nii;
+    private String anio;
+    private String modelo;
+    private String marca;
+    private String motivo;
+    private Integer idTipoUnidad;
+    private String tipo;
+
+    public Unidad() {
+    }
+
+    public Unidad(Integer idUnidad, String vin, String nii, String anio, String modelo, String marca, String motivo, Integer idTipoUnidad, String tipo) {
+        this.idUnidad = idUnidad;
+        this.vin = vin;
+        this.nii = nii;
+        this.anio = anio;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.motivo = motivo;
+        this.idTipoUnidad = idTipoUnidad;
+        this.tipo = tipo;
+    }
+
+    public Integer getIdUnidad() {
+        return idUnidad;
+    }
+
+    public void setIdUnidad(Integer idUnidad) {
+        this.idUnidad = idUnidad;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getNii() {
+        return nii;
+    }
+
+    public void setNii(String nii) {
+        this.nii = nii;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Integer getIdTipoUnidad() {
+        return idTipoUnidad;
+    }
+
+    public void setIdTipoUnidad(Integer idTipoUnidad) {
+        this.idTipoUnidad = idTipoUnidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
