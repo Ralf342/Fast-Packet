0f61b963f1a7ffd87dce1c8731f8ee2590942dff
03jossellin-herrera-rodriguez <josellinherrerarodriguez938@gmail.com>
2024-12-16 23:20:00 -0600

    cliente movil interfaces

--- fastPacket/app/src/main/java/com/example/fastpacket/login.kt
+++ fastPacket/app/src/main/java/com/example/fastpacket/login.kt
@@ -1,6 +1,8 @@
 package com.example.fastpacket
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,22 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // listeners para los TextView
+        findViewById<TextView>(R.id.tvNoTengoCuenta).setOnClickListener {
+            val intent = Intent(this, registro::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<TextView>(R.id.tvOlvideContrasena).setOnClickListener {
+            val intent = Intent(this, recuperarContrasenia::class.java)
+            startActivity(intent)
         }
     }
 }
--- fastPacket/app/src/main/java/com/example/fastpacket/registro.kt
+++ fastPacket/app/src/main/java/com/example/fastpacket/registro.kt
@@ -1,30 +1,20 @@
 package com.example.fastpacket
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class envio : AppCompatActivity() {
+class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_envio)
-
+        setContentView(R.layout.activity_registro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-    }
-    fun onRowClick(view: View) {
-        Toast.makeText(this, "Fila clickeada", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, detalleEnvio::class.java)
-        startActivity(intent)
     }
 }
