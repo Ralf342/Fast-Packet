71a0fb93ec9e8005eb8c49b48d243252f5da008f
Rafael Vázquez Pérez <j.oskar.v282143@gmail.com>
2025-01-09 02:53:16 -0600

    dar de baja unidad

--- FastPacketFX/src/fastpacketfx/modelo/dao/UnidadDAO.java
+++ FastPacketFX/src/fastpacketfx/modelo/dao/UnidadDAO.java
@@ -135,4 +135,24 @@ public class UnidadDAO {
         return msj;
     }
      
+    public static Mensaje darBaja(Unidad unidad){
+        Mensaje msj = new Mensaje();
+        String url = Constantes.URL_wS+"unidad/darBaja";
+        Gson gson = new Gson();
+        try{
+            String parametros = gson.toJson(unidad);
+            RespuestaHTTP respuesta = ConexionWS.peticionPUTJson(url, parametros);
+            if(respuesta.getCodigoRespuesta() == HttpURLConnection.HTTP_OK){
+                msj = gson.fromJson(respuesta.getContenido(), Mensaje.class);
+            }else{
+                msj.setError(true);
+                msj.setMensaje(respuesta.getContenido());
+            }
+        }catch (Exception e){
+           msj.setError(true);
+           msj.setMensaje(e.getMessage());
+        }
+        return msj;
+    } 
+     
 }
